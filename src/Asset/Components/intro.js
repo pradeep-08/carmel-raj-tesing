3647002d2fd5c8dde2421a71b5c41a474552d677
pradeep-08 <114543921+pradeep-08@users.noreply.github.com>
2024-03-29 21:35:25 +0530

    Update intro.js

--- src/Asset/Components/intro.js
+++ src/Asset/Components/intro.js
@@ -19,7 +19,7 @@ function Intro() {
                   {/* <img className='intro-logo-mini-1' src={IntroLogofour} alt="" /> */}
                 {/* <img className='intro-logo-mini-4' width="46" height="46" src="https://img.icons8.com/external-smashingstocks-flat-smashing-stocks/66/external-Android-notification-and-mailing-smashingstocks-flat-smashing-stocks.png" alt="external-Android-notification-and-mailing-smashingstocks-flat-smashing-stocks"/> */}
               <div className="des">
-             <div className="intro-des">An Android developer: expert in Java, crafting seamless mobile experiences with innovative design, performance optimization, and integrated backend solutions.</div>
+             <div className="intro-des">An Android developer: expert in Java,crafting seamless mobile experiences with innovative design, performance optimization, and integrated backend solutions.</div>
             <img className='intro-logo-mini-2' src={IntroLogofive} alt="" />
             </div> 
           <img className='intro-logo-image-1' src={IntroLogoTwo} alt="" />
